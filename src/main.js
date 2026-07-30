561f7a9b6424e4d93e39644f65a0822f6f1d5dee
Chris Wilson <divclass01@gmail.com>
2026-07-30 14:03:53 -0400

    Migrate preview state and refresh board assets

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import { libraryCards } from "./dmView.js?v=npc-lane-1";
 import { rollInitiative, finishTurn } from "./initiative.js";
 import { landingView } from "./landingView.js";
 import { libraryView } from "./libraryView.js?v=npc-lane-1";
-import { loadState, saveState, updateState, findCard } from "./state.js?v=npc-lane-1";
+import { loadState, saveState, updateState, findCard } from "./state.js?v=unified-board-2";
 import { executeCardAction } from "./diceEngine.js";
 import { rollResultView } from "./rollResultView.js";
 import { library } from "./libraryModel.js";
-import { tableView } from "./tableView.js?v=npc-lane-1";
+import { tableView } from "./tableView.js?v=unified-board-2";
 import { handleGameBoardButton } from "./gameBoardController.js";
 
 let state = loadState();
